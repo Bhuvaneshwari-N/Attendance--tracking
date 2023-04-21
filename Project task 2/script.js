a61717908388eec78809265c5b407f578a5ae38d
Bhuvaneshwari N <70068142+Bhuvaneshwari-N@users.noreply.github.com>
2023-04-21 08:40:40 -0700

    Add files via upload

---
+++ Project task 2/script.js
@@ -0,0 +1,28 @@
+function markAttendence(){
+    const attendanceInput = document.getElementById("attendance").value;
+    const attendanceArr = attendanceInput.split(",").map((str) => str.trim());
+
+    //check wheather the number of inputs matches the no of students
+    if (attendanceArr.length !==3){
+        alert("Invalid attendance! Enter the attendance of all the students");
+        return;
+    }
+
+    //Check if the values are valid(present or absent)
+    if(
+        !attendanceArr.every(
+            (value)=> value === "Present" || value=== "Absent" || value === "Late"
+        )
+    ) {
+        alert("Invalid attendance! Enter either Present or Absent or Late");
+        return;
+    }
+
+    //Update Attendance in the table
+    document.getElementById("Rahul-attendance").textContent = attendanceArr[0];
+    document.getElementById("Bhuva-attendance").textContent = attendanceArr[1];
+    document.getElementById("alex-attendance").textContent = attendanceArr[2];
+
+    //Clear the attendance input field
+    document.getElementById("attendance").value = "";
+}
